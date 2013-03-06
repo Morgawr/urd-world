3bcabbc55f1ebb345bf672bce62c3e4436baaf9b
Morgawr <morgawr@gmail.com>
2013-03-06 14:37:55 +0100

    added debug macro and main function

---
+++ include/urd_macros.h
@@ -0,0 +1,7 @@
+
+#ifdef DEBUG
+#define DBG(x) printf x
+#else
+#define DBG(x) 
+#endif 
+
---
+++ src/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <urd_macros.h>
+
+int main(int argc, const char *argv[])
+{
+	return 0;
+}
